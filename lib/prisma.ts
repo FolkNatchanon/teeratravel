05b0ea12eb45f64a2724600c682713ad0be44d71
FolkNatchaon <natchanon.srij@gmail.com>
2026-01-23 22:07:45 +0700

    .

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,14 @@
 import "server-only";
 import { PrismaClient } from "@prisma/client";
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 
 const globalForPrisma = globalThis as unknown as {
     prisma: PrismaClient | undefined;
 };
 
-const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
 
 export const prisma =
     globalForPrisma.prisma ??
     new PrismaClient({
-        adapter,
         log: ["query"],
     });
 
